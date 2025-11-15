051793d3ac6ddf7e07f74f0b5cba06b832738ef6
Hackerguy126578 <135268993+Hackerguy126578@users.noreply.github.com>
2025-11-15 22:05:20 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+return (
+<main style={{ padding: "50px" }}>
+<h1>Welcome to Ali's Airways</h1>
+<p>Your journey begins with comfort, safety, and innovation.</p>
+</main>
+);
+}
